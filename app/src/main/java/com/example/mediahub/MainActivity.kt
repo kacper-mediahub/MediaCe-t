483f6321276488a84fed6ac2f9c7ee723170c1c7
kacper-mediahub <kacper.tablet.2015.bolaz@gmail.com>
2026-08-19 15:30:37 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/mediahub/MainActivity.kt
+++ app/src/main/java/com/example/mediahub/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.DropdownMenu
