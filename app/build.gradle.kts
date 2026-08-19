3ebaa41a83a51d5858964adbd0d6a4d90f07d9a5
kacper-mediahub <kacper.tablet.2015.bolaz@gmail.com>
2026-08-19 15:13:05 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 
 android {
     namespace = "com.example.mediahub"
-
     compileSdk = 35
 
     defaultConfig {
@@ -16,26 +15,19 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
-
     implementation("androidx.activity:activity-compose:1.10.0")
 
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
-
+    implementation("androidx.compose.ui:ui:1.7.6")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
     implementation("androidx.compose.material3:material3:1.3.1")
 
-    implementation("androidx.compose.material:material-icons-extended:1.7.6")
-
-    implementation("androidx.compose.ui:ui:1.7.6")
-
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
-
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.6")
-
-    implementation("androidx.media3:media3-exoplayer:1.5.1")
-
-    implementation("androidx.media3:media3-ui:1.5.1")
 }
